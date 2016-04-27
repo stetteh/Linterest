d0639d809afef6ed70755ba3e5928d07a92eacd0
stetteh <stettehq@gmail.com>
2016-04-26 22:55:19 -0500

    Add project files.

---
+++ Linterest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Linterest.Startup))]
+namespace Linterest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

4c6a4fca89901d7756a410cd1455b3135a5ab586
stetteh <stettehq@gmail.com>
2016-04-26 23:17:29 -0500

    Poco classes defined for user and lins/pins

--- Linterest/Models/IdentityModels.cs
+++ Linterest/Models/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -7,18 +9,21 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace Linterest.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class LinterestUser : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<LinterestUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
         }
+
+        public string Handle { get; set; }
+        public virtual List<Lin> Lins { get; set; } = new List<Lin>();
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<LinterestUser>
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
---
+++ Linterest/Models/Lin.cs
@@ -0,0 +1,11 @@
+﻿namespace Linterest.Models
+{
+    public class Lin
+    {
+        public int Id { get; set; }
+        public LinterestUser Author { get; set; }
+        public  string Text { get; set; }
+        public string ImageUrl { get; set; }
+        public string PinImageUrl { get; set; }       
+    }
+}

29b062b04e76b15fe40c844daac2e3ac75c2f1a6
stetteh <stettehq@gmail.com>
2016-04-30 16:37:04 -0500

    angular controller, factory, module

---
+++ Linterest/App/LinsStuff/LinsController.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('linterestapp')
+        .controller('LinsController', LinsController);
+
+    LinsController.$inject = ['$scope', 'linfactory'];
+
+    function LinsController($scope, linfactory) {
+        /* jshint validthis:true */
+        //var vm = this;
+        $scope.title = 'LinsController';
+
+        activate();
+
+        $scope.saveLin = function () {
+            console.log('called controller saveLin()');
+            var newlin = { ImageUrl: $scope.newLin.ImageUrl, Text: $scope.newLin.Text, PinImageUrl: $scope.newLin.PinImageUrl };
+            linfactory.saveLin(newlin).then(function (res) {
+
+                $scope.newLin.ImageUrl = "";
+                $scope.newLin.Description = "";
+                $scope.newLin.PinImageUrl = "";
+
+                $scope.Lins.push(res.data);
+            });
+        }
+
+
+
+        function activate() {
+            linfactory.getAllLins().then(function (res) {
+                console.log(res.data);
+                $scope.Lins = res.data;
+            });
+        }
+    }
+})();
---
+++ Linterest/App/LinsStuff/factory.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('linterestapp')
+        .factory('linfactory', factory);
+
+    factory.$inject = ['$http'];
+
+    function factory($http) {
+        var service = {
+            getAllLins: getAllLins,
+            saveLin: saveLin
+        };
+
+        return service;
+
+        function getAllLins() {
+            return $http.get('/lins/ShowAllUserPins');
+
+        }
+
+        function saveLin(newlin) {
+            console.log('factory saved lin')
+            return $http.post('/lins/SaveLin', newlin);
+
+        }
+    }
+})();
